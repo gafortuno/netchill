e52c9ba23b3c91914e0834dd8be1a3b8dbc40e53
Gio Adrian Fortuno <fortunogioadrian@gmail.com>
2020-02-23 18:51:49 +0800

    Updates

---
+++ precache-manifest.7fc7d8c80619d629cf494d02b7349d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32722d1b566a5b1ef616cfa42a8d1052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "816aa094b4e21a09dc10",
+    "url": "/static/css/main.c76a07df.chunk.css"
+  },
+  {
+    "revision": "bd9b0e1334f2f6db1c24",
+    "url": "/static/js/2.d53fab20.chunk.js"
+  },
+  {
+    "revision": "d9bee4e26266dab8796c9ed1573a3e49",
+    "url": "/static/js/2.d53fab20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816aa094b4e21a09dc10",
+    "url": "/static/js/main.47d63bd8.chunk.js"
+  },
+  {
+    "revision": "9292e0e2bb70d2944d6b",
+    "url": "/static/js/runtime-main.1aab5399.js"
+  }
+]);
